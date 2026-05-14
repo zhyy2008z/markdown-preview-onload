a80a74d0812d5cb43b803851a9009fdd6fb2c1fd
zhyy2008z <zhyy2008z@outlook.com>
2026-05-14 21:52:28 +0800

    完成第一版功能开发

--- src/extension.ts
+++ src/extension.ts
@@ -2,25 +2,59 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+const PREVIEW_EDITOR_ID = "vscode.markdown.preview.editor";
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	let editModeFiles = new Set(
+		context.workspaceState.get<string[]>("editModeFiles", [])
+	);
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "markdown-preview-onload" is now active!');
+	const currentEditor = vscode.window.activeTextEditor;
+	if (currentEditor) {
+		autoPreview(currentEditor, editModeFiles);
+	}
+}
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('markdown-preview-onload.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from markdown-preview-onload!');
-	});
+/**
+ * 自动重新打开处于编辑状态的文档为预览状态
+ */
+async function autoPreview(editor: vscode.TextEditor, editModeFiles: Set<string>) {
+	if (editor.document.languageId !== "markdown") { return; }
+	if (editor.document.uri.scheme !== "file") { return; }
+	if (editModeFiles.has(editor.document.uri.toString())) { return; }
 
-	context.subscriptions.push(disposable);
+	const uri = editor.document.uri;
+	const viewColumn = editor.viewColumn ?? vscode.ViewColumn.Active;
+	const tab = findTextEditorTabForUri(uri);
+	if (tab) {
+		await vscode.window.tabGroups.close(tab);
+	}
+	await vscode.commands.executeCommand(
+		"vscode.openWith",
+		uri,
+		PREVIEW_EDITOR_ID,
+		viewColumn
+	);
+}
+
+/**
+ * 通过 uri 查找 TextEditorTab
+ */
+function findTextEditorTabForUri(uri: vscode.Uri): vscode.Tab | undefined {
+	const uriStr = uri.toString();
+	for (const group of vscode.window.tabGroups.all) {
+		for (const tab of group.tabs) {
+			if (tab.input instanceof vscode.TabInputText) {
+				if (tab.input.uri.toString() === uriStr) {
+					return tab;
+				}
+			}
+		}
+	}
+	return undefined;
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
